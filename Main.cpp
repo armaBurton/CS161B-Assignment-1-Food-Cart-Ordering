167fad3a1d2b4032df701cd2115cf1dc4af17c01
Arma Burton <arma@armaburton.com>
2023-07-01 17:10:25 -0700

    init commit

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void welcome();
+
+int main(){
+    welcome();
+    
+    
+    system("pause");
+    return 0;
+}
+
+void welcome(){
+    cout << endl;
+    cout << "Welcome to my Food Cart Program!\n";
+    cout << "What would you like to do today?\n";
+}
